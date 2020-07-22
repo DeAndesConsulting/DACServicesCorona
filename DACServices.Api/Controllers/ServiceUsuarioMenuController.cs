e4713d38c49be29fa4b1d660cefb169adfac240e
leo-9 <tec.lrodriguez@gmail.com>
2020-07-22 12:28:46 -0300

    Se agrega controlador para obtener el menu en base al id de usuario y se mockean datos dentro de UserMenuModel para testear en el cliente.

---
+++ DACServices.Api/Controllers/ServiceUsuarioMenuController.cs
@@ -0,0 +1,34 @@
+﻿using DACServices.Api.Models;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DACServices.Api.Controllers
+{
+	[Authorize]
+    public class ServiceUsuarioMenuController : ApiController
+    {
+		private ILog log = LogManager.GetLogger(typeof(ServiceUsuarioMenuController));
+
+		[HttpGet]
+		public object Get(int id)
+		{
+			try
+			{
+				UsuarioMenuModel model = new UsuarioMenuModel();
+				return model.GetMenu();
+			}
+			catch (Exception ex)
+			{
+				log.Error("Error: " + ex.Message);
+				if (ex.InnerException != null)
+					log.Error("Error inner: " + ex.InnerException.Message);
+				throw;
+			}
+		}
+	}
+}
---
+++ DACServices.Api/Models/UsuarioMenuModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DACServices.Api.Models
+{
+	public class UsuarioMenuModel
+	{
+		public List<MenuFather> GetMenu()
+		{
+			List<MenuFather> menu = new List<MenuFather>();
+
+			//creo padre
+			MenuFather menuFather = new MenuFather();
+			menuFather.icon = "mdi-chevron-up";
+			menuFather.icon_alt = "mdi-chevron-down";
+			menuFather.text = "Home";
+			//creo lista hijos
+			menuFather.children = new List<MenuChild>();
+			//creo hijo
+			MenuChild menuChild = new MenuChild();
+			menuChild.text = "Inicio";
+			menuChild.path = "/";
+			//agrego hijo a padre
+			menuFather.children.Add(menuChild);
+				//agrego padre a menu
+			menu.Add(menuFather);
+
+			//creo padre
+			menuFather = new MenuFather();
+			menuFather.icon = "mdi-chevron-up";
+			menuFather.icon_alt = "mdi-chevron-down";
+			menuFather.text = "Seguridad";
+			//creo lista hijos
+			menuFather.children = new List<MenuChild>();
+			//creo hijo
+			menuChild = new MenuChild();
+			menuChild.text = "Usuarios";
+			menuChild.path = "/usuarios";
+			//agrego hijo a padre
+			menuFather.children.Add(menuChild);
+			//creo hijo
+			menuChild = new MenuChild();
+			menuChild.text = "Roles";
+			menuChild.path = "/roles";
+			//agrego hijo a padre
+			menuFather.children.Add(menuChild);
+			//creo hijo
+			menuChild = new MenuChild();
+			menuChild.text = "Perfiles";
+			menuChild.path = "/perfiles";
+			//agrego hijo a padre
+			menuFather.children.Add(menuChild);
+
+			//agrego padre a menu
+			menu.Add(menuFather);
+
+			return menu;
+		}
+	}
+
+	public class MenuFather
+	{
+		public string icon { get; set; }
+		public string icon_alt { get; set; }
+		public string text { get; set; }
+		public bool model { get; set; }
+		public List<MenuChild> children { get; set; }
+ }
+	public class MenuChild
+	{
+		public string text { get; set; }
+		public string path { get; set; }
+	}
+}
