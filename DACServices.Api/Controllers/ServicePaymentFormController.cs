da07592b4930e37369c1728afedef66780212bee
leo-9 <tec.lrodriguez@gmail.com>
2020-08-04 18:54:24 -0300

    Se crea controlador con la firma para generacion del formulario de pago. Falta desarrollar la implementacion de la solicitud con el proveedor.

---
+++ DACServices.Api/Controllers/ServicePaymentFormController.cs
@@ -0,0 +1,37 @@
+﻿using DACServices.Api.Models;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DACServices.Api.Controllers
+{
+	[Authorize]
+    public class ServicePaymentFormController : ApiController
+    {
+		ILog log = LogManager.GetLogger(typeof(ServicePaymentFormController));
+		
+		public object Post(ServicePaymentFormModel model)
+		{
+			try
+			{
+				var sarasa = model.Cuotas;
+
+				model.idPayment = "1";
+				model.urlForm = "http://sarasa";
+
+				return Request.CreateResponse(HttpStatusCode.Created, model);
+			}
+			catch (Exception ex)
+			{
+				log.Error("Error: " + ex.Message);
+				if (ex.InnerException != null)
+					log.Error("Inner exception: " + ex.InnerException.Message);
+				throw new Exception("Error Post, ver log para mas detalle: " + ex.Message);
+			}
+		}
+    }
+}
---
+++ DACServices.Api/Models/ServicePaymentFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DACServices.Api.Models
+{
+	public class ServicePaymentFormModel
+	{
+		public string UserName { get; set; }
+		public string Descripcion { get; set; }
+		public int Monto { get; set; }
+		public int Producto { get; set; }
+		public int Cuotas { get; set; }
+		public string Email { get; set; }
+		public string idPayment { get; set; }
+		public string urlForm { get; set; }
+	}
+}
