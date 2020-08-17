6d79282806da937a3c6c708afbfeed73348c8920
leo-9 <tec.lrodriguez@gmail.com>
2020-08-17 12:15:02 -0300

    Se crea controller Payment para obtener los datos de un pago por id.

---
+++ DACServices.Api/Controllers/ServicePaymentController.cs
@@ -0,0 +1,40 @@
+﻿using DACServices.Business.Service;
+using DACServices.Entities;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DACServices.Api.Controllers
+{
+	public class ServicePaymentController : ApiController
+	{
+		private ILog log = LogManager.GetLogger(typeof(ServicePaymentController));
+		private ServicePaymentBusiness servicePaymentBusiness = new ServicePaymentBusiness();
+
+		[HttpGet]
+		public object Get(int id)
+		{
+			try
+			{
+				bool func(tbPayment x) => x.pay_id == id;
+				var result = servicePaymentBusiness.Read(func) as List<tbPayment>;
+
+				if (result.Count > 0)
+					return Request.CreateResponse(HttpStatusCode.Created, result.FirstOrDefault());
+
+				return Request.CreateErrorResponse(HttpStatusCode.NoContent, "El objeto no existe.");
+			}
+			catch (Exception ex)
+			{
+				log.Error(ex);
+				if (ex.InnerException != null)
+					log.Error("Inner ex: " + ex.InnerException.Message);
+				throw ex;
+			}
+		}
+	}
+}
